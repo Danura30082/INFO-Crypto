4d0080b6015c400fcb00873b6532bd977ad8ae04
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-02-01 15:50:16 +0100

    add reties to find key lenth

--- Vigenere.py
+++ Vigenere.py
@@ -9,21 +9,21 @@ def find_smallest_char(message, key_length = 1):
             smallest_key[letter_index%key_length] = ord(message[letter_index])
     return smallest_key
 
-def find_Vigenere_key_length(message, max_key_length = 100): 
+def find_Vigenere_key_length(message, max_key_length = 100, distance = 1): 
     def gcd(a, b):
         while b != 0:
             a, b = b, a % b
         return a
 
-    def find_peaks(data):
+    def find_peaks(data, distance):
         peaks = []
-        for i in range(1, len(data)-1):  # Skip the first and last element
-            if data[i] > data[i-1] and data[i] > data[i+1]:  # If the current element is a peak
+        for i in range(len(data)):
+            if all(data[i] > data[i+k] for k in range(-distance, distance+1) if i+k < len(data) and k != 0):
                 peaks.append(i)
         if len(peaks) == 0:
             raise ValueError("No peak found in the data")
         return peaks
-
+    fail_count = 0 # number of times the algorithm failed to find a key length
     possible_key_length = []
     for key in range(1,max_key_length):
         repetition = 0
@@ -32,13 +32,18 @@ def find_Vigenere_key_length(message, max_key_length = 100):
                 repetition+=1
         possible_key_length.append(repetition)
     
-    peaks = find_peaks(possible_key_length)
-
+    peaks = find_peaks(possible_key_length, distance)
+    print(peaks)
     gcd_value = peaks[0]+1
     for peak in peaks[1:]:
         gcd_value = gcd(gcd_value, peak+1)
-    if gcd_value == 1:
-        raise ValueError("No possible key length found \n reassess if the message is long enough or try to increase max_key_length \n check if the message is encoded with a Vigenere algorithm")
+    if gcd_value == 1 :
+        if fail_count < 5:
+            fail_count += 1
+            print("No key length found for distance = ",distance," trying distance = ",distance+2)
+            gcd_value = find_Vigenere_key_length(message, max_key_length, distance+2)
+        else:
+            raise ValueError("No key length found \n reassess if the message is long enough or try to increase max_key_length/distance \n check if the message is encoded with a Vigenere algorithm")
     print ("Most likely key length : ",gcd_value)
     return gcd_value
 
@@ -91,11 +96,10 @@ def decode(message, key):
 if __name__ == "__main__":
     path=r'.\Message\message5.txt'
     message = open_file(path)
-    max_key, max_key_length = 11, 5
+    max_key, max_key_length = 11, 100
     try:
         decoded_message, possible_key = Vigenere(message,max_key,max_key_length)
         print(find_most_likely(possible_key))
-        print(possible_key)
         plot(possible_key)
         save(decoded_message)
         plt.pause(1000)
