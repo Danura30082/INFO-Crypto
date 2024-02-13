90fd651549c856a047c6800b8f6f14f819b320a2
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-02-13 14:39:07 +0100

    impented decode and encode for enigma

--- Enigma.py
+++ Enigma.py
@@ -1,17 +1,13 @@
-from rotor_enigma import rotor, rotor_inverser, message_test
+from rotor_enigma import rotor, rotor_inverser
 from common import open_file, save
 
-def increment_position(position, direction=1,rayon = 256):
-    position[0]+=direction
+def increment_position(position, rayon = 256):
+    position[0]+=1
     for i in range(len(position)):
-        if position[i] == rayon and direction == 1:
+        if position[i] == rayon :
             position[i] = 0
             if i != len(position)-1:
                 position[i+1] += 1
-        if position[i] == -1 and direction == -1:
-            position[i] = rayon-1
-            if i != len(position)-1:
-                position[i+1] -= 1
     return position
 
 
@@ -32,13 +28,13 @@ def decode_enigma(rotor_num, position, message, rayon = 256):
     newmessage = ""
     for char in message:
         num_char = ord(char)
-        for i in range(len(rotor_num)):
+        for i in range(len(rotor_num)-1,-1,-1):
             rotor_i = rotor_num[i]
             num_char=(num_char+position[i])%rayon
             num_char=(rotor_inverser[rotor_i][num_char])
             num_char=(num_char-position[i])%rayon
         newmessage += chr(num_char)
-        position = increment_position(position,-1)
+        position = increment_position(position)
     return newmessage
 
 
@@ -47,12 +43,14 @@ def decode_enigma(rotor_num, position, message, rayon = 256):
 
 
 if __name__ == "__main__":
-    clear_message = open_file(r'.\\Message\\decoded_message\\message_7_enigma.txt')
-    message = message_test #open_file(r'.\\Message\\message7_enigma.txt')
+    #check decode and encode working properly
     rotor_num = [0,1,2]
     position_init = [0, 0, 0]
-    encrypted_message= encode_enigma(rotor_num, position_init, clear_message)
-    print(encrypted_message)
-    print(encrypted_message == message)
-
+    clear_message = open_file(r'.\\Message\\decoded_message\\message_7_enigma.txt')
+    from rotor_enigma import message_test
+    assert clear_message == decode_enigma(rotor_num, position_init, message_test)
+    position_init = [0, 0, 0]
+    assert message_test == encode_enigma(rotor_num, position_init, clear_message)
+    
+    
     
