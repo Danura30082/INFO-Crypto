2e64fff7f96bf22a06bb85debbec807112e3e94a
Danura30082 <135959478+Danura30082@users.noreply.github.com>
2024-03-06 15:45:35 +0100

    decode partiel de enigma

--- Enigma.py
+++ Enigma.py
@@ -16,23 +16,24 @@ def multithreaded(rotor_nums):
         # Use map to run the worker function on each rotor_num in parallel
         results = pool.map(worker, args)
     return results
-
-
-def addition(position,b,rayon = 256):
-    retenue
+          
+def addition_long(position, b, rayon = 256):
+    def base(n, base = 256):
+        position = []
+        d = n
+        while d > 0:
+            position.append(d % base)
+            d = d // base
+        return position
+    base_b = base(b, rayon)
+    if len(base_b) > len(position):
+        raise ValueError("b is longer than the position, this is not supported yet.")
+    for i in range(len(base_b)):
+        position[i] += base_b[i]
+    remainder = 0
     for i in range(len(position)):
-        s = position[i] + retenue
-        position[i], retenue = s % rayon, s//rayon
-    return position
-      
-        
-def increment_position(position, rayon = 256):
-    position[0]+=1
-    for i in range(len(position)):
-        if position[i] == rayon :
-            position[i] = 0
-            if i != len(position)-1:
-                position[i+1] += 1
+        s = position[i] + remainder
+        position[i], remainder = s % rayon, s//rayon
     return position
 
 
@@ -45,8 +46,9 @@ def decode_enigma(rotor_num, decode_position, message, rayon = 256):
             num_char=(num_char+decode_position[i])%rayon
             num_char=(rotor_inverser[rotor_i][num_char])
             num_char=(num_char-decode_position[i])%rayon
-        newmessage += chr(num_char)
-        decode_position = increment_position(decode_position)
+        newchar=chr(num_char)
+        newmessage += newchar
+        decode_position = addition_long(decode_position,1)
     return newmessage , decode_position, rotor_num
 
 def brute_force_enigma(message,rotor_num = [0, 1, 2],position = [0]*3,attempts = (256**3)*8*7*6):
@@ -56,7 +58,7 @@ def brute_force_enigma(message,rotor_num = [0, 1, 2],position = [0]*3,attempts =
         if "Joël" in message_test:
             print("\"Joël\" found in the message with position = ",position,"and rotor_num = ",rotor_num)
             return message_test, rotor_num.copy(), position.copy()
-        position = increment_position(position)
+        position = addition_long(position,1)
         if position==[0,0,0]:
             print(rotor_num,"is done")
             return None
@@ -77,17 +79,7 @@ def post_process_results(results):
             rotor_num, position = result[1], result[2]
             
             # Adjust the position based on the length of the decoded message
-            position[0] += len(result[0])
-            
-            # Find the position for the beginning of the message
-            for i in range(3):
-                position[i] = position[i] - length // (256 ** i) % 256
-                if position[i] < 0:
-                    if i < 2:
-                        position[i] += 256
-                        position[i + 1] -= 1
-                    else:
-                        raise ValueError("position is negative in the last digit")
+            position = addition_long(position, 256**3 - length + len(result[0])) #256**3 is the maximum value for a position, so we can use the complement to make a subtraction
                     
             
             # Decode the message using the Enigma machine
@@ -99,8 +91,17 @@ def post_process_results(results):
             # Save the decoded message
             save(result[0])
 
+def check_decode():
+    clear_message = open_file(r'.\\Message\\decoded_message\\message_7_enigma.txt')
+    from rotor_enigma import message_test
+    #check decode working properly
+    rotor_num = [0,1,2]
+    position_init = [0, 0, 0]
+    assert clear_message == decode_enigma(rotor_num, position_init, message_test)[0]
+    print("decode_enigma working properly")
 
 if __name__ == "__main__":
+    check_decode()
     __path__ = r'.\\Message\\message8.txt'
     # Open the last 4 char message file
     message = open_file(__path__)[-4::]
@@ -111,19 +112,20 @@ if __name__ == "__main__":
     # Generate all possible rotor_num permutations
     for loop in range(8*7*6-1):
         # Get the next rotor_num
-        rotor_num = increment_position(rotor_nums[-1].copy(), 8)
+        rotor_num = addition_long(rotor_nums[-1].copy(),1, 8)
         
         # Ensure that the rotor_num does not contain duplicate numbers
         while rotor_num[0] == rotor_num[1] or rotor_num[1] == rotor_num[2] or rotor_num[2] == rotor_num[0]:
-            rotor_num = increment_position(rotor_num, 8)
+            rotor_num = addition_long(rotor_num,1, 8)
         
         # Add the rotor_num to the list
         rotor_nums.append(rotor_num)
         
     # Override the rotor_nums for testing
     rotor_nums = [[4, 1, 7], [5, 1, 7], [6, 1, 7],[7 ,6 ,0],[2 ,3 ,7]]
-    results = multithreaded(rotor_nums)
-        
+    #results = multithreaded(rotor_nums)
+    #print(results)
+    results = [None, ('Joël', [5, 1, 7], [64, 109, 126])]
     # Override the results for testing
     post_process_results(results)
     
